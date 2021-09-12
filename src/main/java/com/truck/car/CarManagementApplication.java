4cd16976ee7b4340671244a2374249e28a3f1642
DESKTOP-0QB8A7R\msi <thetmyatnoe223@gmail.com>
2021-09-12 14:04:47 +0630

    first commit

---
+++ src/main/java/com/truck/car/CarManagementApplication.java
@@ -0,0 +1,13 @@
+package com.truck.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManagementApplication.class, args);
+	}
+
+}
