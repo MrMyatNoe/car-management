dbc09ba71e4550edbe90889251f93109a59718be
KyawSwarheim <kyawswarheim98@gmail.com>
2021-10-31 19:28:25 +0630

    feat:(ksh) test for postgres

--- src/main/java/com/truck/car/model/Test.java
+++ src/main/java/com/truck/car/model/Test.java
@@ -1,10 +1,10 @@
 package com.truck.car.model;
 
 import java.util.UUID;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -15,9 +15,9 @@ import org.hibernate.annotations.GenericGenerator;
 public class Test {
     
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-	@Column(columnDefinition = "BINARY(16)")
+    @GeneratedValue(strategy = GenerationType.IDENTITY,generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+	//@Column(columnDefinition = "BINARY(16)")
     private UUID id;
     private String name;
     private double number;
@@ -29,12 +29,13 @@ public class Test {
     public void setId(UUID id) {
         this.id = id;
     }
+    
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+	public void setName(String name) {
         this.name = name;
     }
 
